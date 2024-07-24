43935161438a9e703d56d9c8e2d7b02a4b5f70d5
Irfan Zainudin <m.irfan.zain@gmail.com>
2024-07-24 23:55:44 +0800

    added cjawi, [WIP] pending rtl support for vscode

---
+++ كاموس.h
@@ -0,0 +1,57 @@
+#ifndef كاموس
+#define كاموس
+
+// PRIMITIVE DATA TYPES
+#define حروف char
+#define اكسارا char         // synonym / sinonim
+#define دوا double
+#define ڤچهن float
+#define نومبور int
+#define نوم int             // abbreviation / singkatan
+#define ڤنجڠ long
+#define ڤينديق short
+
+// KEYWORDS
+// auto
+#define keluar break
+#define kes case
+#define tetap const
+#define malar const         // synonym / sinonim
+#define langkau continue
+#define lalai default
+#define buat do
+#define اتاو else
+#define bancian enum
+#define banci enum          // abbreviation / singkatan
+#define pengangkaan         // synonym / sinonim
+#define angka enum          // abbreviation / singkatan
+#define perhitungan enum    // synonym / sinonim
+#define hitung enum         // abbreviation / singkatan
+// extern
+#define اونتوق for
+#define gerak goto
+#define pergi goto          // synonym / sinonim
+#define کالاو if
+// register
+#define ڤولڠ return
+// signed
+// sizeof
+#define statik static
+#define kaku static         // synonym / sinonim
+#define struktur struct
+#define struk struct    // abbreviation / singkatan
+#define tukar switch
+// typedef
+// union
+// unsigned
+#define kosong void
+// volatile
+#define سلاݢي while
+
+
+// FUNCTIONS
+#define اوتام main
+#define چيتق printf
+#define ايمبس scanf
+
+#endif
